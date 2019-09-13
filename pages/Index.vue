cb246cf427c31a6406ff1d5f97ab0cf715dc24ed
wyf <wei.yinfu@qq.com>
2019-09-13 10:35:13 +0800

    good

---
+++ pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container class="Index">
+    <el-header>
+      This is header
+    </el-header>
+    <el-main class="main">
+      this is main
+    </el-main>
+    <el-footer height="30">This is Footer</el-footer>
+  </el-container>
+</template>
+<script>  
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  }
+};
+</script>
+<style lang="less">
+@import url("../common.less");
+
+.Index {
+  .fillParent();
+  .myFrame();
+}
+</style>
